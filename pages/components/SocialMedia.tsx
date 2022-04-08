a2b9fe3c66d0c162ca6669e1af98b8976c502ae4
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-04-08 15:23:00 -0500

    Show icons in large screen when sidebar is hidden
    SocialMedia component CSS doesn't depend on sideBar state, and it
    behaves the same whether sideBar is showing or hiding.
    
    Also minor css improvements related to box-shadow and hover on project
    item component.

--- pages/components/SocialMedia.tsx
+++ pages/components/SocialMedia.tsx
@@ -1,14 +1,10 @@
-import { useContext } from 'react'
 import styles from '../../styles/Home.module.css'
-import { openSidebarContext } from '../../lib/OpenSidebarContext'
 import Icons from './Icons'
 
 const SocialMedia = (): JSX.Element => {
-  const { hideSidebar } = useContext(openSidebarContext)
-
   return (
     <aside id='socialMediaLinks'>
-      <div className={hideSidebar ? `${styles.socialMedia} ${styles.side}` : `${styles.socialMedia} ${styles.hidden}`}>
+      <div className={`${styles.socialMedia} ${styles.side}`}>
         <Icons />
       </div>
     </aside>
