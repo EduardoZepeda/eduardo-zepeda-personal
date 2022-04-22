44bee17cb6d87c83fd46f676f39fe1d7f4f3c873
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-04-21 21:56:16 -0500

    Change sitemap for sitemap Protocol example

--- generateSitemap.js
+++ generateSitemap.js
@@ -4,13 +4,16 @@ let date = new Date()
 
 async function generateSitemap() {
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://eduardozepeda.dev</loc>
-    <lastmod>${date.toISOString().split('T')[0]}</lastmod>
-  </url>
-<urlset/>`
+  const sitemap = `<?xml version="1.0" encoding="utf-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <url>
+          <loc>https://eduardozepeda.dev/</loc>
+          <lastmod>${date}</lastmod>
+
+      </url>
+  </urlset>`
   fs.writeFileSync('public/sitemap.xml', sitemap)
 }
 
