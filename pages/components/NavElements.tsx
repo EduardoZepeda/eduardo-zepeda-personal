ee3dc89a8f67cb224e5a7ebf9ee465c0b0a30923
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-04-27 10:53:50 -0500

    Add underline effect to nav links

--- pages/components/NavElements.tsx
+++ pages/components/NavElements.tsx
@@ -1,16 +1,17 @@
 import { useContext } from 'react'
 import { openSidebarContext } from '../../lib/OpenSidebarContext'
+import styles from '../../styles/Home.module.css'
 
 const NavElements = (): JSX.Element => {
   const { setHideSidebar } = useContext(openSidebarContext)
 
   return (
     <ol>
-      <ul><a onClick={() => setHideSidebar(true)} href='#'>Introduction</a></ul>
-      <ul><a onClick={() => setHideSidebar(true)} href='#about'>About me</a></ul>
-      <ul><a onClick={() => setHideSidebar(true)} href='#portfolio'>Portfolio</a></ul>
-      <ul><a onClick={() => setHideSidebar(true)} href='#projects'>OSS</a></ul>
-      <ul><a onClick={() => setHideSidebar(true)} href='#contact'>Contact</a></ul>
+      <ul><a className={styles.navLink} onClick={() => setHideSidebar(true)} href='#'>Introduction</a></ul>
+      <ul><a className={styles.navLink} onClick={() => setHideSidebar(true)} href='#about'>About me</a></ul>
+      <ul><a className={styles.navLink} onClick={() => setHideSidebar(true)} href='#portfolio'>Portfolio</a></ul>
+      <ul><a className={styles.navLink} onClick={() => setHideSidebar(true)} href='#projects'>OSS</a></ul>
+      <ul><a className={styles.navLink} onClick={() => setHideSidebar(true)} href='#contact'>Contact</a></ul>
     </ol>
   )
 }
