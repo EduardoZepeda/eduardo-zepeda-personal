98f4bd8d90ab100e72871df87b69a1b312369f88
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-08-08 01:15:22 +0100

    update sitemap

--- generateSitemap.js
+++ generateSitemap.js
@@ -1,8 +1,27 @@
 const fs = require('fs')
+const matter = require('gray-matter')
 
 const date = new Date()
 
 async function generateSitemap () {
+  let content = ''
+  try {
+    const files = fs.readdirSync('public/blog/content/posts')
+    const rawPosts = files.map((directory) => {
+      const readFile = fs.readFileSync(`public/blog/content/posts/${directory}/index.en.md`, 'utf-8')
+      const { data: frontmatter } = matter(readFile)
+      return {
+          slug: directory,
+          frontmatter
+      }
+    })
+    // sort posts by date
+    .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+    .filter(post => process.env.NODE_ENV === 'production' ? !post.frontmatter.draft : true)
+    rawPosts.forEach(({slug, frontmatter})=>content+=`<url><loc>https://eduardozepeda.dev/blog/${slug}/</loc><lastmod>${frontmatter.date}</lastmod></url>`)
+  } catch (e) {
+    console.error(e)
+  }
   const sitemap = `<?xml version="1.0" encoding="utf-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -10,10 +29,10 @@ async function generateSitemap () {
       <url>
           <loc>https://eduardozepeda.dev/</loc>
           <lastmod>${date}</lastmod>
-
       </url>
+      ${content}
   </urlset>`
   fs.writeFileSync('public/sitemap.xml', sitemap)
-}
 
+}
 generateSitemap()
