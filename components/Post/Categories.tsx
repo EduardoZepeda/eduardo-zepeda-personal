2320ffefd83c069fcf095c94eaeab334e82b3159
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-08-18 23:18:05 +0100

    Minimal css changes

--- components/Post/Categories.tsx
+++ components/Post/Categories.tsx
@@ -5,7 +5,7 @@ const Categories = ({ categories }: CategoriesProps): JSX.Element => {
     const id = useId()
     return (
         <div className={styles.categories}>
-            {categories.map((category, index) => <span key={`${id}-${index}`} className={styles.category}>{category}</span>)}
+            {categories.map((category, index) => <div key={`${id}-${index}`} className={styles.category}>{category}</div>)}
         </div>
     )
 }
