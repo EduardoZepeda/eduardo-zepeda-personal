764b7a52388d5b21ddce7e05413b6d134563d6e0
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-08-19 18:09:14 +0100

    Lazy load mermaid and remove useless packages from prod

--- components/MermaidLazy.tsx
+++ components/MermaidLazy.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { CodeProps } from 'react-markdown/lib/ast-to-react'
 import { useInView } from 'react-intersection-observer'
+import { Mermaid } from 'mermaid'
+
+const cache = new Map<string, Mermaid>()
 
 const MermaidLazy = ({ className, children, ...props }: React.HTMLProps<HTMLElement>) => {
     const { ref, inView, entry } = useInView({
@@ -8,19 +10,21 @@ const MermaidLazy = ({ className, children, ...props }: React.HTMLProps<HTMLElem
         triggerOnce: true,
     })
 
-    async function loadMermaid() {
-        const { default: mermaid } = await import('mermaid')
-        mermaid.initialize({ startOnLoad: false, theme: 'dark' });
-        await mermaid.run({
+    async function initializeMermaid() {
+        if (!cache.get('mermaid')) {
+            const { default: mermaid } = await import('mermaid')
+            cache.set('mermaid', mermaid)
+        }
+        cache.get('mermaid')?.initialize({ startOnLoad: false, theme: 'dark' });
+        await cache.get('mermaid')?.run({
             querySelector: '.language-mermaid',
         })
     }
 
     if (inView) {
-        loadMermaid()
+        initializeMermaid()
     }
 
-    console.log("loaded")
     return (
         <code {...props} ref={ref} className={className}>
             {children}
