bde85cc358c7d4798ae02a2a0bfbf7c13501f0bd
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-10-25 17:32:02 +0100

    Reduce about me size

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = (): JSX.Element => {
     <section id='about'>
       <h2>About me</h2>
       <div className={styles.aboutMe}>
-        <p>I value <strong>mature and well tested</strong> technologies over brand new frameworks: Django, DRF, React, NextJS, Bootstrap, Go, Hugo.
+        <p>I value <strong>mature and battle-tested</strong> technologies over brand new frameworks: Django, DRF, React, NextJS, Express, Bootstrap, Go, Hugo, etc.
         </p>
         <p>I've been using GNU/Linux for <strong>more than 10 years.</strong></p>
         <p>I'm a big fan of Digital Ocean and Azure, Microsoft <a href='https://www.credly.com/badges/17608a52-2cb7-4268-a907-613459559911/public_url' rel='noreferrer noopener' target='_blank' aria-label='AZ-900 certification link'>AZ-900 certified.</a></p>
