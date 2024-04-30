a2357501df349af728e9d312f80ea35a06953034
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2024-04-30 14:18:17 -0600

    Generate sitemap manually and writing it to public/

--- generateSitemap.ts
+++ generateSitemap.ts
@@ -1,16 +1,26 @@
-import { siteData } from "siteData";
+import fs from 'fs'
+import { siteData } from 'siteData'
 
-export default async function generateSitemap() {
-  const date = new Date();
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${siteData['siteUrl']}</loc>
-    <lastmod>${date.toISOString().split("T")[0]}</lastmod>
-  </url>
-  <url>
-    <loc>${siteData['siteUrl']}/blog</loc>
-    <lastmod>${date.toISOString().split("T")[0]}</lastmod>
-  </url>
-<urlset/>`;
+async function generateSitemap() {
+
+  const sitemap = `<?xml version="1.0" encoding="utf-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <url>
+          <loc>${siteData["siteUrl"]}</loc>
+          <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.9</priority>
+      </url>
+      <url>
+          <loc>${siteData["siteUrl"]}/blog/</loc>
+          <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.9</priority>
+      </url>
+  </urlset>`
+  fs.writeFileSync('public/sitemap.xml', sitemap)
 }
+
+export default generateSitemap
