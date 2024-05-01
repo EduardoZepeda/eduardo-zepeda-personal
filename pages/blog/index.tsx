7a7654359dd6083057d8e490d6a82b427dd1665a
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2024-04-30 18:32:09 -0600

    Add meta tags based on siteData to blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,6 +3,7 @@ import styles from '@styles/blog.module.css';
 import extractUrlsFromRSS from '@utils/parseXml/parseXML';
 import { siteData } from 'siteData';
 import mainStyles from '@styles/Home.module.css'
+import Head from 'next/head';
 
 export const getStaticProps = (async () => {
     const url = siteData["blog"]["rss"]
@@ -81,11 +82,27 @@ export default function Blog({ data }: { data: PostFromXml[] }) {
     return (
         <div>
             <h1>My latests posts</h1>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content={siteData["blog"]["description"]} />
+                <meta name="author" content={fullName} />
+                <meta property="og:locale" content="en_US" />
+                <meta name="og:description" content={siteData["blog"]["description"]} />
+                <meta property="og:type" content="profile" />
+                <meta property="profile:first_name" content={siteData["authorFirstName"]} />
+                <meta property="profile:last_name " content={siteData["authorLastName"]} />
+                <meta property="profile:username" content={siteData["username"]} />
+                <meta property="profile:gender" content={siteData["gender"]} />
+                <meta property="og:url" content={siteData["siteUrl"]} />
+                <meta property="og:title" content={`My latests posts | ${fullName}`} />
+                <meta property="twitter:title" content={`My latests posts | ${fullName}`} />
+                <meta property="twitter:description" content={siteData["blog"]["description"]} />
+            </Head>
             <div className={styles.container}>
                 <p>{`My posts are hosted on my blog `}<strong>{`where I've published ${data.length} so far. `}</strong>{`Posts are ordered cronologically feel free to dive into one of them.`}</p>
                 <ul>
                     {data?.slice(0, NUMBER_OF_POSTS).map(({ link, title, summary, pubDate, category }) => (
-                        <li key={link}>
+                        <li className={styles.postLi} key={link}>
                             <Link title={title} aria-label={`Link to ${title}`} href={link}>
                                 <h2 className={styles.postTitle}>{title}</h2>
                                 <small className={styles.pubDate}><ol className={styles.categories}>{typeof category === "string" ? <li className={styles.category}>{category}</li> : category.map(cat => <li className={styles.category} key={title + cat}>{cat}</li>)}</ol></small>
